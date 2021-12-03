978ef53ab7adbdc622fb5113a2ad6c67b1447a66
Will Eccles <will@eccles.dev>
2021-12-03 11:40:41 -0500

    day 1 part 2

--- src/day01.cpp
+++ src/day01.cpp
@@ -1,15 +1,48 @@
 #include <iostream>
 
+// This is gross.
+
 int main(void) {
-  int prev, val;
-  int inc = 0;
-  std::cin >> prev;
-  while (std::cin >> val) {
-    if (val > prev) {
-      ++inc;
+  int vals[3];
+  int idx = 0;
+  int psum = 0;
+  int pval = 0;
+  int count1 = 0;
+  int count2 = 0;
+
+  std::cin >> vals[0] >> vals[1] >> vals[2];
+
+  // Preprocess the first 3 values for part 1
+  pval = vals[0];
+  for (int i = 1; i < 3; ++i) {
+    if (vals[i] > pval) {
+      ++count1;
     }
-    prev = val;
+    pval = vals[i];
   }
-  std::cout << "part 1: " << inc << '\n';
+
+  // Preprocess the first 3 values for part 2
+  psum = vals[0] + vals[1] + vals[2];
+
+  while (std::cin >> vals[idx]) {
+    // Part 1
+    if (vals[idx] > pval) {
+      ++count1;
+    }
+    pval = vals[idx];
+
+    // Part 2
+    int sum = vals[0] + vals[1] + vals[2];
+    if (sum > psum) {
+      ++count2;
+    }
+    psum = sum;
+    idx = (idx + 1) % 3;
+  }
+
+  std::cout
+      << "part 1: " << count1 << '\n'
+      << "part 2: " << count2 << '\n';
+
   return 0;
 }
